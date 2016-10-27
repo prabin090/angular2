5be9b9b485b6bd356047cf9a64c4af4f3ef04f5e
Prabin Kumar Swatipali <pswatipali @knowledgeinfotech.com>
2016-10-27 16:22:39 +0530

    changing css

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -5,7 +5,7 @@ import { Location }                 from '@angular/common';
 
 import { HeroService } from './hero.service';
 import {ModalModule} from "ng2-modal";
-import { PushNotificationsService, SimpleNotificationsModule, NotificationsService } from 'angular2-notifications';
+import { PushNotificationsService, SimpleNotificationsModule, NotificationsService, Options } from 'angular2-notifications';
 import {HeroesComponent} from './heroes.component';
 import {ProgressPieChartService} from './progresspiechart.service'
 
@@ -25,15 +25,24 @@ export class HeroDetailComponent implements OnInit {
         private route: ActivatedRoute,
         private location: Location,
         private _pushNotifications: PushNotificationsService,
-        private _notificationsService: NotificationsService,
-        private _service: SimpleNotificationsModule,
+        // private _notificationsService: NotificationsService,
+        // private _service: SimpleNotificationsModule,
         private herocomponent: HeroesComponent,
-        private progresspie: ProgressPieChartService
+        private progresspie: ProgressPieChartService,
+        private _service: NotificationsService,
+        private _push: PushNotificationsService
 
     ) { }
 
+    public title: string = 'just a title';
+    public content: string = 'just content';
+    public type: string = 'success';
+
+    public deleteId: string;
+
+    public temp: boolean[] = [true, false];
     ngOnInit(): void {
-console.log(this.herocomponent.myvalue)
+
         this.route.params.forEach((params: Params) => {
             let id = +params['id'];
             this.heroService.getHero(id)
@@ -55,13 +64,48 @@ console.log(this.herocomponent.myvalue)
     goBack(): void {
         this.location.back();
     }
-    public options = {
-        position: ["left"],
-        timeOut: 5000,
-        lastOnBottom: true
 
+    public options = {
+        timeOut: 5000,
+        lastOnBottom: true,
+        clickToClose: true,
+        maxLength: 0,
+        maxStack: 7,
+        showProgressBar: true,
+        pauseOnHover: true,
+        preventDuplicates: false,
+        preventLastDuplicates: 'visible',
+        rtl: false,
+        animate: 'scale',
+        position: ['right', 'bottom']
+    };
+    create() {
+        switch (this.type) {
+            case 'success':
+                let a = this._service.success(this.title, this.content, { id: 123 });
+                break;
+            case 'alert':
+                this._service.alert(this.title, this.content);
+                break;
+            case 'error':
+                this._service.error(this.title, this.content);
+                break;
+            case 'info':
+                this._service.info(this.title, this.content);
+                break;
+            case 'bare':
+                this._service.bare(this.title, this.content);
+                break;
+        }
     }
 
+    onCreate(event: any) {
+        console.log(event);
+    }
+
+    onDestroy(event: any) {
+        console.log(event);
+    }
 
 }
 
